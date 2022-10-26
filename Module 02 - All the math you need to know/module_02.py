7c56a37f070bd66d9d4e12fe9058f1cb4df4123a
Michael Lanoy <74580854+Tachy0nx@users.noreply.github.com>
2022-10-26 09:53:32 -0600

    sections 2 and 3
    labs done in lecture for sections 2 and 3

---
+++ Module 02 - All the math you need to know/module_02.py
@@ -0,0 +1,15 @@
+#prompts user for a number value
+#num = int(input('Pick a number '))
+
+#int(num)
+#determines if the numebr is odd or even
+#if num % 2 == 0:
+#    result == 'even'
+#else:
+#    result == 'odd'
+#prints the result
+#print(result)
+
+#for num in range(1,11):
+#    if not x % 2:
+#        print(x)
---
+++ Module 03 - Strings and Other Types/module_03.py
@@ -0,0 +1,7 @@
+#msg = '1,2,3,4,5,6,7,8,9,10'
+#numbers = msg.split(',')
+
+#zmsg = "hello world"
+#chars = list(msg)
+#chars[0] = 'H'
+#msg = "".join(chars)
